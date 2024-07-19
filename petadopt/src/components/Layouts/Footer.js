e971a314ba2800bc2080a612c0e6bc725b47c05f
j27000 <namanjhala32@gmail.com>
2024-07-19 12:54:16 -0400

    footer

--- petadopt/src/components/Layouts/Footer.js
+++ petadopt/src/components/Layouts/Footer.js
@@ -3,10 +3,10 @@ export const Footer = () => {
   return (
 
 
-    <footer className="bg-white rounded-lg shadow dark:bg-gray-900 ">
-      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+    <footer className="bg-white rounded-lg shadow dark:bg-gray-900 mb-0 ">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8 mb-0">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+          <a href="https://flowbite.com/" className="flex items-center mb-4 mb-0 space-x-3 rtl:space-x-reverse">
             <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
           </a>
