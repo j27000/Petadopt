b9603e9532537be1357e8a1ce6b3672a2eed1318
j27000 <namanjhala32@gmail.com>
2024-07-23 14:25:19 -0400

    .

--- petadopt/src/pages/Cart/components/CartCard.js
+++ petadopt/src/pages/Cart/components/CartCard.js
@@ -1,30 +1,29 @@
-
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { useCart } from "../../../Context/Contextpet";
 
 export const CartCard = ({ pet }) => {
     const { removeFromCart } = useCart();
-    function handleclick(pet) {
-        removeFromCart(pet)
-
-    }
 
     return (
-        <div className="flex flex-wrap justify-between border-b dark:border-slate-700 max-w-4xl m-auto p-2 mb-5 ">
+        <div className="flex flex-wrap justify-between border-b dark:border-slate-700 max-w-4xl m-auto p-2 mb-5">
             <div className="flex">
-                <Link >
+                <Link to={`${pet.id}`}>
                     <img className="w-32 rounded" src={pet.imageUrl} alt={pet.name} />
                 </Link>
-                <div >
-                    <Link >
-                        <p className="text-lg ml-2 dark:text-slate-200">{pet.name}</p>
+                <div>
+                    <Link to={`${pet.id}`}>
+                        <p className="text-lg ml-2 ">{pet.name}</p>
                     </Link>
-                    <button onClick={handleclick(pet)} className="text-base ml-2 text-red-400">Remove</button>
+                    <button
+                        onClick={() => removeFromCart(pet.id)}
+                        className="text-base ml-2 text-red-400">
+                        Remove
+                    </button>
                 </div>
             </div>
-            <div className="text-lg m-2 dark:text-slate-200">
+            <div className="text-lg m-2 ">
                 <span>{pet.description}</span>
             </div>
         </div>
-    )
-}
+    );
+};
