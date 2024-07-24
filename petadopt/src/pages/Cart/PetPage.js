a440672231169c072497b447ba6eb351d1de939f
j27000 <namanjhala32@gmail.com>
2024-07-24 13:35:52 -0400

    .

--- petadopt/src/pages/Cart/PetPage.js
+++ petadopt/src/pages/Cart/PetPage.js
@@ -1,19 +1,13 @@
-import { useCart } from "../../Context/Contextpet"
-import { CartEmpty } from "./components/CartEmpty"
-import { List } from "./components/List"
+import { useCart } from "../../Context/Contextpet";
+import { CartEmpty } from "./components/CartEmpty";
+import { List } from "./components/List";
 
 export const PetPage = () => {
-
     const { petList } = useCart();
-    let Cartlength = 1;
-    if (petList.length === 0) {
-        Cartlength = 0;
-    }
+
     return (
         <main>
-            {Cartlength ? <List /> : <CartEmpty />}
-
-
+            {petList.length === 0 ? <CartEmpty /> : <List />}
         </main>
-    )
-}
+    );
+};
